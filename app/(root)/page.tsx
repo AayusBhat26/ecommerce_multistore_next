2e57ca1bcf66def51d326129b17949387987065f
AayusBhat26 <100667276+AayusBhat26@users.noreply.github.com>
2023-08-21 08:06:56 +0530

    routing auth setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+      return (
+            // <p>hello this is admin dashboard</p>
+            <div>
+                  <Button >Click me</Button>
+            </div>
+      )
+}
